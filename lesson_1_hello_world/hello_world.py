6847e7919f8906eef2a4a24db3937598807ddcf0
Christian Lowe <imchristianlowe@gmail.com>
2021-06-11 01:09:26 -0400

    add hello world with test

---
+++ lesson_1_hello_world/hello_world.py
@@ -0,0 +1,9 @@
+import requests
+
+
+def hello_world():
+    return "Hello world"
+
+
+if __name__ == '__main__':
+    hello_world()
---
+++ lesson_1_hello_world/tests/test_hello_world.py
@@ -0,0 +1,12 @@
+from lesson_1_hello_world.hello_world import hello_world
+import unittest
+
+
+class TestHelloWorld(unittest.TestCase):
+
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_hello_world(self):
+        r = hello_world()
+        self.assertEqual(r, "Hello world")
